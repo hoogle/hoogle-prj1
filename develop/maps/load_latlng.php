588f5a346e9be0e25560c78353cd05324ce9ed7e
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2009-03-24 17:18:13 +0000

    modify table column name and modify sql condition

--- develop/maps/load_latlng.php
+++ develop/maps/load_latlng.php
@@ -4,7 +4,10 @@
   require LIBRARY_PATH."function.inc";
 
   Connect_Mysql();
-  $lalnAry = getListData('web3.map_point', $total, array("userid" => $_SESSION['userid']), "userid"); 
+  //$lalnAry = getListData('web3.map_point', $total, array("userid" => $_SESSION['userid']), "curr_time", "desc"); 
+  $sql = "SELECT * FROM web3.map_point WHERE userid = '{$_SESSION['userid']}' AND lat between '{$_GET['miny']}' AND '{$_GET['maxy']}' AND lng between '{$_GET['minx']}' AND '{$_GET['maxx']}'";
+  $res = Query_Mysql($sql);
+  while($lalnAry[] = mysql_fetch_assoc($res));
   Close_Mysql();
   header("Content-Type: application/json");
   echo json_encode($lalnAry);
