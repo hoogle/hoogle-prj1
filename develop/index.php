616a7b4b2da744ec6a21998af8db5e6e704b63b6
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2009-04-22 16:20:02 +0000

    update

--- develop/index.php
+++ develop/index.php
@@ -5,26 +5,30 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>TravelMap</title>
-<link rel="stylesheet" href="http://yui.yahooapis.com/2.6.0/build/reset-fonts-grids/reset-fonts-grids.css" type="text/css">
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.0.0pr2/build/cssreset/reset-min.css">
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.0.0pr2/build/cssfonts/fonts-min.css">
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.0.0pr2/build/cssgrids/grids.css">
 <link rel="stylesheet" type="text/css" href="/photos/css/layout.css" />
+<style type="text/css">
+.yui-d0 div {
+    text-align:center;
+}
+</style>
 </head>
 
 <body>
-<div class="yui-t7" style="width:100%;">
   <div id="hd">
 <? require WEBROOT_PATH."include/header.php"; ?>
   </div>
 
   <div id="bd">
-    <div class="yui-g">
-      <div style="width:800px;height:500px;margin:0 auto;background-color:white;">
-        <div style="font:bold 30pt Verdana;color:#ccc;padding-top:200px;">Comming soon...</div>
-      </div>
+    <div class="yui-d3 contain-main">
+          Comming soon...
     </div>
   </div>
+
   <div id="ft">
 <? require WEBROOT_PATH."include/footer.php"; ?>
   </div>
-</div>
 </body>
 </html>
--- develop/upload/do_upload.php
+++ develop/upload/do_upload.php
@@ -3,15 +3,45 @@
   require LIBRARY_PATH."function.inc";
   Connect_Mysql();
   $userid = $_POST['userid'];
+  $pid = $_POST['pid'];
+  $title = $_POST['title'];
+  $desc = $_POST['desc'];
 
-  //print_r($_POST);
-  //echo "userid : {$userid}";
-  $userPicAry = getListData("web3.album", $acnt, array("userid" => $userid));
-  $book = (!$acnt) ? initAlbum($userid) : $_POST['book'];
+  $userPicAry = getListData("web3.photo", $acnt, array("userid" => $userid));
+  if (!$acnt) initAlbum($userid);
+  $target_book_path = getUserPath($userid).USER_MAX_BOOKID;
+  $target_thumb_path = "{$target_book_path}/thumb";
+  $target_org_img = "{$target_book_path}/{$pid}.jpg";
+  $target_thumb_img = "{$target_thumb_path}/t{$pid}.jpg";
+  $target_tile_img = TILE_UPLOAD_PATH."t{$pid}.jpg";
+
   foreach ($_FILES as $fieldName => $file)
   { 
+    $tmp_file = $file['tmp_name'];
     $file_name = str_replace('..', '', $file['name']); 
     $file_name = str_replace('/', '', $file_name); 
-    move_uploaded_file($file['tmp_name'], getUserPath($userid)."{$book}/{$file_name}"); 
+
+    $cmd = sprintf("/usr/local/bin/convert %s -thumbnail '90x90^' -gravity center -extent 90x90 %s", $tmp_file, $target_thumb_img);
+    system($cmd);
+
+    $cmd = sprintf("/usr/local/bin/jpegtopnm %s | /usr/local/bin/pnmscale -width=%d -height=%d | /usr/local/bin/ppmtojpeg > %s", $tmp_file, ICON_SIZE, ICON_SIZE, $target_tile_img);
+    system($cmd);
+
+    $cmd = sprintf("/usr/local/bin/convert -resize '%dx%d>' '%s' '%s'", 550, 550, $tmp_file, $target_org_img);
+    system($cmd);
+
+    //$cmd = sprintf("/usr/local/bin/jpegtopnm %s | /usr/local/bin/pnmscale -width=%d | /usr/local/bin/ppmtojpeg > %s", $tmp_file, 90, $target_thumb_img);
+    //system($cmd);
+    //move_uploaded_file($tmp_file, $target_org_img);
   } 
+  $dataAry = array(
+    "userid" => $userid,
+    "book" => USER_MAX_BOOKID,
+    "title" => $title,
+    "description" => $desc,
+    "post_time" => "now()",
+    "point_id" => $pid
+  ); 
+  print_r($dataAry);
+  insertData("web3.photo", $dataAry, $photo_id); 
 ?>
