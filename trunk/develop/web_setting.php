d4c94dd4ae01624777a457d46933b9754d8b8fff
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2009-04-27 17:19:22 +0000

    modify UPLOAD_PATH

--- trunk/develop/web_setting.php
+++ trunk/develop/web_setting.php
@@ -2,8 +2,8 @@
   #session_start();
   define("WEBROOT_PATH", "/home/www/develop/");
   define("LIBRARY_PATH", "/home/www/lib/");
-  define("USER_UPLOAD_PATH", "/mnt/photos/upload/user/");
-  define("TILE_UPLOAD_PATH", "/mnt/photos/upload/tile/");
+  define("USER_UPLOAD_PATH", "/mnt/filer/photos/upload/user/");
+  define("TILE_UPLOAD_PATH", "/mnt/filer/photos/upload/tile/");
   define('ICON_SIZE', 30);
   define('USER_MAX_BOOKID', 32767);
 ?>
