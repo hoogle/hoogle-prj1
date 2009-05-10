284193fde141577f900d4b54906b4e132c051524
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2009-05-10 17:19:23 +0000

    add define PHOTO_PATH

--- develop/web_setting.php
+++ develop/web_setting.php
@@ -1,9 +1,10 @@
 <?
   #session_start();
-  define("WEBROOT_PATH", "/home/www/develop/");
-  define("LIBRARY_PATH", "/home/www/lib/");
-  define("USER_UPLOAD_PATH", "/mnt/filer/photos/upload/user/");
-  define("TILE_UPLOAD_PATH", "/mnt/filer/photos/upload/tile/");
+  define('WEBROOT_PATH', '/home/www/develop/');
+  define('LIBRARY_PATH', '/home/www/lib/');
+  define('PHOTO_PATH', '/mnt/filer/');
+  define('USER_UPLOAD_PATH', '/mnt/filer/photos/upload/user/');
+  define('TILE_UPLOAD_PATH', '/mnt/filer/photos/upload/tile/');
   define('ICON_SIZE', 30);
   define('USER_MAX_BOOKID', 32767);
 ?>
