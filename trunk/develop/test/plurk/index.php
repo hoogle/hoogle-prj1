4bb0caba96fb5d773a1231365b9369ef942741be
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2009-12-18 18:39:17 +0000

    add get_plurk.php get.php

--- trunk/develop/test/plurk/index.php
+++ trunk/develop/test/plurk/index.php
@@ -12,21 +12,39 @@
     nick: <input type="text" id="nick"/><br/>
     pwd: <input type="password" id="pwd"/><br/>
     msg: <input type="text" id="msg"/><br/>
-    <input type="button" id="getit" value="get it!"/>
-    <div id="show"></div>
+    <input type="button" id="plurkit" value="plurk it!"/>
+    <div id="stat"></div>
+</div>
+<div>
+    <input type="button" id="getit" value="get it!"/> 
+    <div id="show"></div> 
+    <div id="nick"></div>
+    <div id="karma"></div>
 </div>
 </body>
 </html>
 <script type="text/javascript">
-    $('#getit').click(function(e) {
+    $('#plurkit').click(function(e) {
         var nick = $('#nick').val();
         var pwd = $('#pwd').val();
         var msg = $('#msg').val();
-        $('#show').html('發送中...');
+        $('#stat').html('發送中...');
         $.post('post2plurk.php', {nick:nick, pwd:pwd, msg:msg}, function(data) {
             if (data['error'] == null) {
-                $('#show').html('發送成功!!');
+                $('#stat').html('發送成功!!');
             }
+        }, 'json');
+    });
+
+    $('#getit').click(function() {
+        $('#show').html('geting...');
+        $.post('get_plurk.php', {nick:'hoogle', pwd:'qUs4obog', uid:'3982321'}, function(data) {
+        var uids = "";
+        for(var item in data['plurk_users']) {
+            uids+= '<br/>' + item;
+        }
+        $('#show').html('users: '+uids);
+    console.dir(data);
         }, 'json');
     });
 </script>
