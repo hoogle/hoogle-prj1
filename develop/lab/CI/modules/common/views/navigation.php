5bcdb7b1969ce12246583e6319cb7b711ee721fb
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-02-11 11:26:39 +0000

    rearrange coding conventaion

--- develop/lab/CI/modules/common/views/navigation.php
+++ develop/lab/CI/modules/common/views/navigation.php
@@ -5,58 +5,60 @@ $main_items = array(
 );
 $option_items = array(
     'list_all' => array('url' => '/lang/list_all', 'text' => 'List all'),
-    'add' => array('url' => '/lang/add', 'text' => 'New one'),
+    'add' => array('url' => '/lang/add', 'text' => 'New one', 'items' => array('url' => 'xxx', 'text' => 'OOO')),
 );
 $menu_items = ( ! $lang_arr) ? $main_items : array_merge($main_items, $option_items);
 
-?><div id="navigation" class="yui-menu yui-menu-horizontal yui-menubuttonnav">
+?>
+<div id="navigation" class="yui-menu yui-menu-horizontal yui-menubuttonnav">
     <div class="yui-menu-content">
         <ul>
-           <?php
-                $i = 0;
-                foreach ($menu_items as $index => $data) :
-                    $i++;
-                    $class = '';
-                    $selected = '';
-                    if ($file_name.'.html' === $data['url']) :
-                        $selected = ' on';
-                    endif;
-                    switch (true) :
-                        case ($i===1) :
-                            $class = ' first-of-type';
-                            break;
-                    endswitch;
-            ?><li<?php echo (isset($data['items'])) ? '' : ' class="yui-menuitem"'; ?>>
-                <?php if (isset($data['items'])) : ?><a class="yui-menu-label <?php echo $index; ?><?php echo $class; ?>" href="<?php echo $data['url']; ?>"><em><?php echo $data['text']; ?></em></a>
-                <?php else : ?><a class="yui-menuitem-content <?php echo $index; ?><?php echo $class; ?>" href="<?php echo $data['url']; ?>"><?php echo $data['text']; ?></a>
-                <?php endif; ?>
-
-                <?php if (isset($data['items'])) : ?><div class="yui-menu">
+<?php
+        $i = 0;
+        foreach ($menu_items as $index => $data) :
+            $i++;
+            $class = '';
+            $selected = '';
+            if ($file_name.'.html' === $data['url']) :
+                $selected = ' on';
+            endif;
+            switch (true) :
+                case ($i===1) :
+                    $class = ' first-of-type';
+                    break;
+            endswitch;
+?>
+            <li<?php echo (isset($data['items'])) ? '' : ' class="yui-menuitem"'; ?>>
+<?php       if (isset($data['items'])) : ?>
+                <a class="yui-menu-label <?php echo $index; ?><?php echo $class; ?>" href="<?php echo $data['url']; ?>"><em><?php echo $data['text']; ?></em></a>
+<?php       else : ?>
+                <a class="yui-menuitem-content <?php echo $index; ?><?php echo $class; ?>" href="<?php echo $data['url']; ?>"><?php echo $data['text']; ?></a>
+<?php       endif; ?>
+<?php       if (isset($data['items'])) : ?>
+                <div class="yui-menu">
                     <div class="yui-menu-content">
                         <ul>
-                            <?php
-                                $y = 0;
-                                foreach ($data['items'] as $sub_index => $sub_data) :
-                                    $y++;
-                                    $sub_class = '';
-                                    switch (true) :
-                                        case ($y===1) :
-                                            $sub_class = ' first-of-type';
-                                            break;
-                                    endswitch;
-                            ?><li class="yui-menuitem">
+<?php
+                $y = 0;
+                foreach ($data['items'] as $sub_index => $sub_data) :
+                    $y++;
+                    $sub_class = '';
+                    switch (true) :
+                        case ($y===1) :
+                            $sub_class = ' first-of-type';
+                            break;
+                    endswitch;
+?>
+                            <li class="yui-menuitem">
                                 <a class="yui-menuitem-content" href="<?php echo $sub_data['url']; ?>"><?php echo $sub_data['text']; ?></a>
                             </li>
-                            <?php endforeach ?>
-
+<?php           endforeach ?>
                         </ul>
                     </div>
                 </div>
-                <?php endif ?>
-
+<?php       endif ?>
             </li>
-            <?php endforeach ?>
-
+<?php   endforeach ?>
         </ul>
     </div>
 </div>
--- develop/lab/CI/modules/l10n/views/index.php
+++ develop/lab/CI/modules/l10n/views/index.php
@@ -1,15 +1,15 @@
 <?php $this->load->view("common/before_body.php"); ?>
 <div id="hd">
-<div class="yui-d3">
+    <div class="yui-d3">
 <?php $this->load->view("common/masthead.php"); ?>
 <?php $this->load->view("common/navigation.php"); ?>
-</div>
+    </div>
 </div>
 <div id="bd">
-<div class="yui-d3">
-<div class="yui-t4">
-<div class="yui-main">
-<div class="yui-b">
+    <div class="yui-d3">
+        <div class="yui-t4">
+            <div class="yui-main">
+                <div class="yui-b">
 <?php if ($div == "show") : ?>
 <?php echo modules::run("lang/index"); ?>
 <?php elseif ($div == "add") : ?>
@@ -21,18 +21,17 @@
 <?php else : ?>
 <?php $this->load->view("l10n/home"); ?>
 <?php endif ?>
-</div>
-</div>
-<div class="yui-b">
+                </div>
+            </div>
+            <div class="yui-b">
 <?php $this->load->view("l10n/sidebar"); ?>
-</div>
-</div>
-</div>
-</div>
+            </div><!-- .yui-b (end)  -->
+        </div><!-- .yui-main (end) -->
+    </div><!-- .yui-t4 (end) -->
+</div><!-- #bd (end) -->
 <div id="ft">
-<div class="yui-d3">
+    <div class="yui-d3">
 <?php $this->load->view("common/mastfoot"); ?>
-</div>
-</div>
+    </div>
 </div>
 <?php $this->load->view("common/after_body.php"); ?>
