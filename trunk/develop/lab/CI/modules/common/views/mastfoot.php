d2178fcb0fadf9095b5930d94048d30336957a22
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-03-02 09:58:55 +0000

    modify build function

--- trunk/develop/lab/CI/modules/common/views/mastfoot.php
+++ trunk/develop/lab/CI/modules/common/views/mastfoot.php
@@ -1,6 +1,6 @@
 <div id="masthead">
     <div class="mod-content">
-        <h2>#mastfoot</h2>
-        <p>This is footer area!</p>
+        <h2>Muchiii &copy; 2010</h2>
+        <p>Powered by D-Link</p>
     </div>
 </div>
