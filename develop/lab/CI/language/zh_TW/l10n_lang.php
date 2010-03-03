c2c1e4fd792c70c7e62770620ae9dffc5f84ff46
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-03-03 03:15:47 +0000

    modify quote to double quote

--- develop/lab/CI/language/zh_TW/l10n_lang.php
+++ develop/lab/CI/language/zh_TW/l10n_lang.php
@@ -1,14 +1,14 @@
 <?php
-$lang['l10n_MUCHIII_MEMBER_PROFILE_total_albums'] = '共有 {%total_photo%} 本相簿';
-$lang['l10n_MUCHIII_MEMBER_PROFILE_total_photos'] = '共有 {%total_photo%} 張照片';
-$lang['l10n_LOGIN_you_have_to_login'] = '請登入111';
-$lang['l10n_LOGIN_logout'] = '登出';
-$lang['l10n_LOGIN_login'] = '登入';
-$lang['l10n_LOGIN_welcome'] = '歡迎';
-$lang['l10n_LOGIN_hihi'] = '嗨嗨';
-$lang['l10n_TOOLBAR_WISHLIST'] = '願望清單';
-$lang['l10n_one'] = '一';
-$lang['l10n_two'] = '二';
-$lang['l10n_three'] = '三';
-$lang['l10n_hoogle'] = '虎哥!';
+$lang['l10n_MUCHIII_MEMBER_PROFILE_total_albums'] = "共有 {%total_photo%} 本相簿";
+$lang['l10n_MUCHIII_MEMBER_PROFILE_total_photos'] = "共有 {%total_photo%} 張照片";
+$lang['l10n_LOGIN_you_have_to_login'] = "請登入";
+$lang['l10n_LOGIN_logout'] = "登出";
+$lang['l10n_LOGIN_login'] = "登入";
+$lang['l10n_LOGIN_welcome'] = "歡迎";
+$lang['l10n_LOGIN_hihi'] = "嗨嗨";
+$lang['l10n_TOOLBAR_WISHLIST'] = "願望清單";
+$lang['l10n_one'] = "一";
+$lang['l10n_two'] = "二";
+$lang['l10n_three'] = "三";
+$lang['l10n_hoogle'] = "虎哥!";
 ?>
