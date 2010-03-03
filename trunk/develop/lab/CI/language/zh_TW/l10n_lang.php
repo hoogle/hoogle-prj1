140a4a792a5392887b1fd58f39f9bd0488ffa7b5
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-03-03 03:35:49 +0000

    fix single and double quote bug

--- trunk/develop/lab/CI/language/zh_TW/l10n_lang.php
+++ trunk/develop/lab/CI/language/zh_TW/l10n_lang.php
@@ -1,7 +1,7 @@
 <?php
 $lang['l10n_MUCHIII_MEMBER_PROFILE_total_albums'] = "共有 {%total_photo%} 本相簿";
 $lang['l10n_MUCHIII_MEMBER_PROFILE_total_photos'] = "共有 {%total_photo%} 張照片";
-$lang['l10n_LOGIN_you_have_to_login'] = "請登入";
+$lang['l10n_LOGIN_you_have_to_login'] = "請登入!3";
 $lang['l10n_LOGIN_logout'] = "登出";
 $lang['l10n_LOGIN_login'] = "登入";
 $lang['l10n_LOGIN_welcome'] = "歡迎";
