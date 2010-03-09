1698ed7ea786fa5af21927f9d2fdaf9136eb6045
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-03-09 11:12:02 +0000

    api ok

--- trunk/develop/lab/CI/modules/sharebox/controllers/delfile.php
+++
@@ -1,7 +0,0 @@
-<form name="F" method="post" action="http://122.116.58.213:8080/api/deleteFiles" enctype="multipart/data">
-<input type="file" name="del_files[]" />
-<input type="file" name="del_files[]" />
-<input type="file" name="del_files[]" />
-<input type="file" name="del_files[]" />
-<input type="submit" value="send"/>
-</form>
--- trunk/develop/lab/CI/modules/sharebox/views/index.php
+++ trunk/develop/lab/CI/modules/sharebox/views/index.php
@@ -8,8 +8,11 @@
 <script type="text/javascript" src="http://yui.yahooapis.com/combo?2.8.0/build/yahoo-dom-event/yahoo-dom-event.js&2.8.0/build/connection/connection-min.js&2.8.0/build/element/element-min.js&2.8.0/build/button/button-min.js"></script>
 <style type="text/css">
 ul li {
-    list-style-type:circle;
-    margin:5px;
+    list-style-type:square;
+    border:1px solid #aaccff;
+    background-color:#ddeeff;
+    margin:10px;
+    padding:5px;
 }
 .input_text {
     width:100px;
@@ -32,19 +35,54 @@ ul li {
         </div>
         <div class="yui-b">
             <ul>
+                <li><input type="button" id="get_version" value=" GET Version "/></li>
+                <li><input type="button" id="get_router" value=" GET Router "/></li>
+                <li><input type="button" id="get_devices" value=" GET Devices "/></li>
                 <li><input type="button" id="get_filelist" value=" GET Filelist "/></li>
                 <li>
-                    <input type="text" id="dirname" class="input_text"/>
+                    <input type="text" id="dirname" class="input_text" value="new_<?php echo date("mdHi"); ?>"/>
                     <input type="button" id="create_dir" value=" CREATE Directory "/>
                 </li>
                 <li>
                     <form name="upform" method="post" enctype="multipart/form-data">
                         <input type="file" id="browse_file" name="browse_file" class="input_text"/><br/>
-                        <input type="button" id="upfile" value=" upload file "/> 
+                        <input type="button" id="upfile" value=" UPLOAD File "/> 
                     </form>
                 </li>
                 <li>
-                    <input type="button" id="del_files" value=" DELETE files "/>
+                    <form id="delF" name="delF" method="post" action="http://122.116.58.213:8080/api/deleteFiles">
+                        <input type="text" name="files[]" value="one"/><br/>
+                        <input type="text" name="files[]" value="two"/><br/>
+                        <input type="text" name="files[]" value="three"/><br/>
+                        <input type="text" name="files[]" value="four"/><br/>
+                        <input type="text" name="path" value="./"/>
+                        <input type="button" id="del_files" value=" DELETE Files "/>
+                    </form>
+                </li>
+                <li>
+                    <input type="button" id="rename_file" value=" RENAME Files "/>
+                </li>
+                <li>
+                    <form id="cpF" name="cpF" method="post" action="http://122.116.58.213:8080/api/copyFilesTo">
+                        <input type="text" name="files[]" value="file1"/><br/>
+                        <input type="text" name="files[]" value="file2"/><br/>
+                        <input type="text" name="files[]" value="file3"/><br/>
+                        <input type="text" name="files[]" value="file4"/><br/>
+                        <input type="text" name="src_path" value="./"/><br/>
+                        <input type="text" name="dst_path" value="./YKK/"/>
+                        <input type="button" id="copy_files" value=" COPY Files "/>
+                    </form>
+                </li>
+                <li>
+                    <form id="mvF" name="mvF" method="post" action="http://122.116.58.213:8080/api/moveFilesTo">
+                        <input type="text" name="files[]" value="file1"/><br/>
+                        <input type="text" name="files[]" value="file2"/><br/>
+                        <input type="text" name="files[]" value="file3"/><br/>
+                        <input type="text" name="files[]" value="file4"/><br/>
+                        <input type="text" name="src_path" value="./"/><br/>
+                        <input type="text" name="dst_path" value="./YKK/"/>
+                        <input type="button" id="move_files" value=" MOVE Files "/>
+                    </form>
                 </li>
             </ul>
         </div>
@@ -54,14 +92,38 @@ ul li {
 </html>
 <script type="text/javascript">
 var $ = YAHOO.util.Dom.get;
-var PB1 = new YAHOO.widget.Button("get_filelist");
-var PB2 = new YAHOO.widget.Button("create_dir");
-var PB3 = new YAHOO.widget.Button("upfile");
-var PB4 = new YAHOO.widget.Button("del_files");
-PB1.on('click', function() {
+var PB0 = new YAHOO.widget.Button("get_version");
+var PB1 = new YAHOO.widget.Button("get_router");
+var PB2 = new YAHOO.widget.Button("get_devices");
+var PB3 = new YAHOO.widget.Button("get_filelist");
+var PB4 = new YAHOO.widget.Button("create_dir");
+var PB5 = new YAHOO.widget.Button("upfile");
+var PB6 = new YAHOO.widget.Button("del_files");
+var PB7 = new YAHOO.widget.Button("rename_file");
+var PB8 = new YAHOO.widget.Button("copy_files");
+var PB9 = new YAHOO.widget.Button("move_files");
+
+PB0.on('click', function() {
+    var url = 'http://122.116.58.213:8080/api/getVersion?sig=6t2eUYr23R&expires=123234235&routerAccessKeyId=WE32w4hjk3er46&t=' + new Date().valueOf();
     YAHOO.util.Connect.asyncRequest(
         'GET',
-        'http://122.116.58.213:8080/api/getFileList?path=./&t=' + new Date().valueOf(),
+        url,
+        {
+            success: function(o) {
+                $('result').innerHTML = o.responseText;
+            },
+            failure: function(o) {
+                $('result').innerHTML = o.statusText;
+            }
+        }
+    );
+});
+
+PB1.on('click', function() {
+    var url = 'http://122.116.58.213:8080/api/getRouter?sig=6t2eUYr23R&expires=123234235&routerAccessKeyId=WE32w4hjk3er46&t=' + new Date().valueOf();
+    YAHOO.util.Connect.asyncRequest(
+        'GET',
+        url,
         {
             success: function(o) {
                 $('result').innerHTML = o.responseText;
@@ -74,7 +136,7 @@ PB1.on('click', function() {
 });
 
 PB2.on('click', function() {
-    var url = 'http://122.116.58.213:8080/api/createDirectory?path=./&dirname='+$('dirname').value+'&t=' + new Date().valueOf();
+    var url = 'http://122.116.58.213:8080/api/listShareDevices?sig=6t2eUYr23R&expires=123234235&routerAccessKeyId=WE32w4hjk3er46&t=' + new Date().valueOf();
     YAHOO.util.Connect.asyncRequest(
         'GET',
         url,
@@ -90,6 +152,37 @@ PB2.on('click', function() {
 });
 
 PB3.on('click', function() {
+    YAHOO.util.Connect.asyncRequest(
+        'GET',
+        'http://122.116.58.213:8080/api/getFileList?path=./&t=' + new Date().valueOf(),
+        {
+            success: function(o) {
+                $('result').innerHTML = o.responseText;
+            },
+            failure: function(o) {
+                $('result').innerHTML = o.statusText;
+            }
+        }
+    );
+});
+
+PB4.on('click', function() {
+    var url = 'http://122.116.58.213:8080/api/createDirectory?path=./&dirname='+$('dirname').value+'&t=' + new Date().valueOf();
+    YAHOO.util.Connect.asyncRequest(
+        'GET',
+        url,
+        {
+            success: function(o) {
+                $('result').innerHTML = o.responseText;
+            },
+            failure: function(o) {
+                $('result').innerHTML = o.statusText;
+            }
+        }
+    );
+});
+
+PB5.on('click', function() {
     if ($('browse_file').value == "") {
         alert('browse upload file, please');
     } else {
@@ -115,7 +208,30 @@ PB3.on('click', function() {
     }
 });
 
-PB4.on('click', function() {
-    window.location.href="http://122.116.58.213/lab/delfile.php";
+PB6.on('click', function() {
+    $('delF').submit();
+});
+
+PB7.on('click', function() {
+    YAHOO.util.Connect.asyncRequest(
+        'GET',
+        'http://122.116.58.213:8080/api/renameFile?src_fullfilename=./log-2010-03-05.php&dst_filename=log-20100305.php&t=' + new Date().valueOf(),
+        {
+            success: function(o) {
+                $('result').innerHTML = o.responseText;
+            },
+            failure: function(o) {
+                $('result').innerHTML = o.statusText;
+            }
+        }
+    );
+});
+
+PB8.on('click', function() {
+    $('cpF').submit();
+});
+
+PB9.on('click', function() {
+    $('mvF').submit();
 });
 </script>
