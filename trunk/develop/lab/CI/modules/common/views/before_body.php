6e2a44c1f978fc47cdfc751ea40bf23fdb85500d
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-05-21 03:43:48 +0000

    update yui 2.7.0 to 2.8.1 on js src

--- trunk/develop/lab/CI/modules/common/views/before_body.php
+++ trunk/develop/lab/CI/modules/common/views/before_body.php
@@ -3,10 +3,10 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 <title>Localization</title>
-<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/combo?3.0.0/build/cssreset/reset-min.css&3.0.0/build/cssfonts/fonts-min.css&3.0.0/build/cssgrids/grids-min.css&2.7.0/build/tabview/assets/skins/sam/tabview.css&2.7.0/build/paginator/assets/skins/sam/paginator.css&2.7.0/build/datatable/assets/skins/sam/datatable.css"/>
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/combo?3.0.0/build/cssreset/reset-min.css&3.0.0/build/cssfonts/fonts-min.css&3.0.0/build/cssgrids/grids-min.css&2.8.1/build/tabview/assets/skins/sam/tabview.css&2.8.1/build/paginator/assets/skins/sam/paginator.css&2.8.1/build/datatable/assets/skins/sam/datatable.css"/>
 <link rel="stylesheet" type="text/css" href="/static/node-menunav.css">
 <link rel="stylesheet" type="text/css" href="/static/muchiii.css">
-<script type="text/javascript" src="http://yui.yahooapis.com/combo?3.0.0/build/yui/yui-min.js&2.7.0/build/yahoo-dom-event/yahoo-dom-event.js&2.7.0/build/element/element-min.js&2.7.0/build/tabview/tabview-min.js&2.7.0/build/connection/connection-min.js&2.7.0/build/json/json-min.js&2.7.0/build/datasource/datasource-min.js&2.7.0/build/datatable/datatable-min.js&2.7.0/build/paginator/paginator-min.js&2.7.0/build/get/get-min.js"></script>
+<script type="text/javascript" src="http://yui.yahooapis.com/combo?3.0.0/build/yui/yui-min.js&2.8.1/build/yahoo-dom-event/yahoo-dom-event.js&2.8.1/build/element/element-min.js&2.8.1/build/tabview/tabview-min.js&2.8.1/build/connection/connection-min.js&2.8.1/build/json/json-min.js&2.8.1/build/datasource/datasource-min.js&2.8.1/build/datatable/datatable-min.js&2.8.1/build/paginator/paginator-min.js&2.8.1/build/get/get-min.js"></script>
 <script type="text/javascript" src="/static/muchiii.js"></script>
 </head>
 <body class="yui-skin-sam">
