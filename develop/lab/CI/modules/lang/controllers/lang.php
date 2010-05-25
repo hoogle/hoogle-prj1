68f2189c8a24d106f986069649898c30a8a0c78e
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-05-25 07:05:12 +0000

    add lang function import

--- develop/lab/CI/modules/lang/controllers/lang.php
+++ develop/lab/CI/modules/lang/controllers/lang.php
@@ -1,6 +1,7 @@
 <?php
 class Lang extends Controller {
     var $_browser_lang = "";
+    var $_import_file_path = "/tmp/lang/";
 
     function __construct()
     {
@@ -9,15 +10,23 @@ class Lang extends Controller {
 
     function get_uselang($lang = NULL)
     {
-        if ( ! is_null($lang))
+        if ( ! is_null($lang) && ! empty($lang))
         {
             $this->_browser_lang = $lang;
         }
         else
         {
-            $accept_lang_arr = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
-            list($lang_lang, $lang_country) = explode("-", $accept_lang_arr[0]);
-            $this->_browser_lang = (is_null($lang_country)) ? "en_US" : strtolower($lang_lang)."_".strtoupper($lang_country);
+            $this->load->library("session");
+            if ( ! is_null($this->session->userdata("use_lang")))
+            {
+                $this->_browser_lang = $this->session->userdata("use_lang");
+            }
+            else
+            {
+                $accept_lang_arr = explode(",", $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+                list($lang_lang, $lang_country) = explode("-", $accept_lang_arr[0]);
+                $this->_browser_lang = (is_null($lang_country)) ? "en_US" : strtolower($lang_lang)."_".strtoupper($lang_country);
+            }
         }
     }
 
@@ -42,7 +51,7 @@ class Lang extends Controller {
 
     function changes()
     {
-        $lang = $this->_browser_lang;
+        $this->get_uselang($this->_browser_lang);
         $this->load->library("session");
         $userid = $this->session->userdata("user_id");
         $lang_arr = $this->session->userdata("lang_perm");
@@ -50,8 +59,8 @@ class Lang extends Controller {
         {
             $data = array(
                 "userid" => NULL,
-                "go_url" => ",lang,changes",
-                "lang_arr" => NULL 
+                "lang_arr" => NULL,
+                "go_url" => ",lang,changes"
             );
             $this->load->library("layout", "layout_main");
             $this->layout->view("login/please_login", $data);
@@ -72,7 +81,7 @@ class Lang extends Controller {
             }
             $data = array(
                 "rec" => $rec,
-                "use_lang" => $lang,
+                "use_lang" => $this->_browser_lang,
                 "userid" => $userid,
                 "lang_arr" => $lang_arr,
             );
@@ -83,7 +92,7 @@ class Lang extends Controller {
 
     function edit($sid)
     {
-        $this->get_uselang();
+        $this->get_uselang($this->_browser_lang);
         $this->load->library("session");
         $userid = $this->session->userdata("user_id");
         $lang_arr = $this->session->userdata("lang_perm");
@@ -251,27 +260,87 @@ class Lang extends Controller {
 
     function export()
     {
-        $this->load->database();
-        $this->load->model("l10n_model");
+        $this->get_uselang($this->_browser_lang);
         $this->load->library("session");
-        $this->_browser_lang = $this->session->userdata("use_lang");
-        $params = array (
-            "use_lang" => $this->_browser_lang,
-        );
-        $res = $this->l10n_model->get_all_lang($params, $total);
-
-        $outstr = "";
-        foreach ($res as $k => $arr)
+        $userid = $this->session->userdata("user_id");
+        $lang_arr = $this->session->userdata("lang_perm");
+        $this->load->library("layout", "layout_main");
+        if ($userid === FALSE)
         {
-            $outstr.= "{$arr['key_word']}|{$arr['translate']}\n";
+            $data = array(
+                "userid" => NULL,
+                "lang_arr" => NULL,
+                "go_url" => ",lang,export",
+            );
+            $this->layout->view("login/please_login", $data);
         }
-        $outstr = iconv("UTF-8", "big5//IGNORE", $outstr);
-        header("Content-Type: application/force-download");
-        header("Content-Type: application/octet-stream");
-        header("Content-Type: text/csv");
-        header("Content-disposition: inline; filename=\"lang_{$this->_browser_lang}.csv\";");
-        echo $outstr;
-        exit;
+        else
+        {
+            $this->load->database();
+            $this->load->model("l10n_model");
+            $params = array (
+                "use_lang" => $this->_browser_lang,
+            );
+            $res = $this->l10n_model->get_all_lang($params, $total);
+
+            $outstr = "";
+            foreach ($res as $k => $arr)
+            {
+                $outstr.= "{$arr['key_word']}|{$arr['translate']}\n";
+            }
+            $outstr = iconv("UTF-8", "big5//IGNORE", $outstr);
+            header("Content-Type: application/force-download");
+            header("Content-Type: application/octet-stream");
+            header("Content-Type: text/csv");
+            header("Content-disposition: inline; filename=\"lang_{$this->_browser_lang}.csv\";");
+            echo $outstr;
+            exit;
+        }
+    }
+
+    function import()
+    {
+        $this->get_uselang($this->_browser_lang);
+        $this->load->library("session");
+        $userid = $this->session->userdata("user_id");
+        $lang_arr = $this->session->userdata("lang_perm");
+        $this->load->library("layout", "layout_main");
+        if ($userid === FALSE)
+        {
+            $data = array(
+                "userid" => NULL,
+                "lang_arr" => NULL,
+                "go_url" => ",lang,import",
+            );
+            $this->layout->view("login/please_login", $data);
+        }
+        else
+        {
+            $data = array(
+                "userid" => $userid,
+                "use_lang" => $this->_browser_lang,
+                "lang_arr" => $lang_arr,
+            );
+            $this->layout->view("lang/import", $data);
+        }
+    }
+
+    function do_imp()
+    {
+        $config['upload_path'] = $this->_import_file_path;
+        $config['allowed_types'] = "csv";
+        $filename = preg_replace("/^(.*)\.(\w+)$/", "\\1", $_FILES['importFile']['name'])."_".date("His");
+        $config['file_name'] = $filename;
+        $this->load->library("upload", $config);
+        if ( ! $this->upload->do_upload("importFile"))
+        {
+            echo "error: {$this->upload->error_msg}";
+        }
+        else
+        {
+            $this->upload->data();
+            header("location:/lang/list_all");
+        }
     }
 
     function listit($lang = NULL)
@@ -298,13 +367,14 @@ class Lang extends Controller {
         $this->load->view("lang/listit", $data);
     }
 
-    function gen($lang=null)
+    function gen($lang = NULL)
     {
+        $this->get_uselang($lang);
         $this->load->library("session");
         $userid = $this->session->userdata("user_id");
+        $lang_arr = $this->session->userdata("lang_perm");
         $this->load->database();
         $this->load->model("l10n_model");
-        $lang_arr = $this->session->userdata("lang_perm");
 
         if ($userid === FALSE)
         {
@@ -336,7 +406,7 @@ class Lang extends Controller {
                 }
                 $gen_str.= "?>\n";
                 $filepath = APPPATH."language/{$lang_item["l_type"]}/l10n_lang.php";
-                file_put_contents($filepath, $gen_tr);
+                file_put_contents($filepath, $gen_str);
             }
             header("location:/lang/list_all");
             exit;
