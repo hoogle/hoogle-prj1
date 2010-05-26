3b39355c5229e8808de5ecd62f6acf46b0567e2c
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-05-26 05:31:44 +0000

    modify add.php form button

--- develop/lab/CI/modules/lang/views/add.php
+++ develop/lab/CI/modules/lang/views/add.php
@@ -1,9 +1,17 @@
-<form name="Fi" method="post" action="/lang/ins">
+<form id="Fadd" name="Fi" method="post" action="/lang/ins">
   <ul>
     <li>key: <input type="text" name="key_word"/></li>
 <?php foreach($lang_arr as $lang_item) : ?>
     <li><?php echo $lang_item['l_name']?>: <input type="text" name="<?php echo $lang_item['l_type']?>_word"/></li>
 <?php endforeach ?>
   </ul>
-  <input type="submit" name="send" value=" ADD NEW "/>
+  <input type="button" id="btn_new" name="send" value=" ADD NEW "/>
 </form>
+<script type="text/javascript">
+var $ = YAHOO.util.Dom.get,
+    PB = new YAHOO.widget.Button("btn_new");
+
+PB.on('click', function() {
+    $('Fadd').submit();
+});
+</script>
