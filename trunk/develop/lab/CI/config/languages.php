f667a64a3b3b152353e172887701c1502a622037
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-06-04 03:44:47 +0000

    remove l10n's file into l10n's folder

---
+++ trunk/develop/lab/CI/config/languages.php
@@ -0,0 +1,32 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang_arr = array(
+    "en_US" => array(
+        "l_id" => 1,
+        "l_type" => "en_US",
+        "bit_id" => 1,
+        "l_name" => "English (US)",
+        "create_time" => "2010-01-26 15:32:28",
+        "update_time" => "2010-01-26 15:32:28",
+        "inherit_id" => 0,
+    ),
+    "zh_TW" => array(
+        "l_id" => 2,
+        "l_type" => "zh_TW",
+        "bit_id" => 2,
+        "l_name" => "Chinese (Taiwan)",
+        "create_time" => "2010-01-26 15:32:34",
+        "update_time" => "2010-01-26 15:32:34",
+        "inherit_id" => 0,
+    ),
+    "en_IN" => array(
+        "l_id" => 3,
+        "l_type" => "en_IN",
+        "bit_id" => 4,
+        "l_name" => "English (India)",
+        "create_time" => "2010-01-26 15:33:56",
+        "update_time" => "2010-01-26 15:33:56",
+        "inherit_id" => 1,
+    ),
+);
+?>
---
+++ trunk/develop/lab/CI/config/page_cate.php
@@ -0,0 +1,21 @@
+<?php
+$page_arr = Array (
+    "MIIICASA" => Array
+        (
+            "INDEX" => Array
+                (
+                    "HOME" => Array
+                        (
+                            "PHOTOS" => 0
+                        ),
+                    "DEVICE" => Array
+                        (
+                            "ROUTER" => 0,
+                            "USB" => 0
+                        ),
+                    "SHARE" => 0
+                ),
+        ),
+    "PROFILE" => 0
+);
+?>
