a183f75c3e16136c10291d331046ece1c7d5cd24
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-06-08 08:11:12 +0000

    move common view to l10n/view

---
+++ trunk/develop/lab/CI/modules/l10n/views/l10n/body.php
@@ -0,0 +1,20 @@
+<body class="yui-skin-sam">
+<div id="doc3" class="yui-t5">
+  <div id="hd" role="banner"><h1>Localization tool</h1></div>
+  <div id="bd" role="main">
+    <div id="yui-main">
+      <div class="yui-b">
+          <div class="yui-g">
+            <?php echo modules::run("l10n/lang/index", $lang); ?>
+          </div>
+      </div>
+    </div>
+
+    <div class="yui-b">
+      <?php $this->load->view("l10n/login/login"); ?>
+    </div>
+  </div>
+  <div id="ft" role="contentinfo"><p>Footer</p></div>
+</div>
+</body>
+</html>
---
+++ trunk/develop/lab/CI/modules/l10n/views/l10n/mastfoot.php
@@ -0,0 +1,6 @@
+<div id="masthead">
+    <div class="mod-content">
+        <h2>Muchiii &copy; 2010</h2>
+        <p>Powered by D-Link</p>
+    </div>
+</div>
