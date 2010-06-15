b91dbb6f3ca19f7ec3e7cd5b9afffa3613b816c1
hoogle.wang <hoogle.wang@fd0b497e-0abd-11de-9c5c-7db7966a26d7>
2010-06-15 06:11:39 +0000

    change array to return

--- trunk/develop/lab/CI/config/languages.php
+++ trunk/develop/lab/CI/config/languages.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$lang_arr = array(
+return array(
     "en_US" => array(
         "l_id" => 1,
         "l_type" => "en_US",
